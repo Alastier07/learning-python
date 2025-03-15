5ff9739879ca6cdcfe4543fec0fc0bbd95e4de16
Alastier <calastier07@gmail.com>
2025-03-15 22:11:03 +0800

    update: basic python

--- basic.py
+++ basic.py
@@ -78,3 +78,72 @@ else:
 
 #any, all
 
+#complex numbers
+num1 = 2+3j
+num2 = complex(2, 3)
+
+print(num2.real, num2.imag)
+
+print(abs(-5.5)) #5.5
+print(round(5.6)) #6
+print(round(5.59, 1)) #5.6
+
+#Enums
+
+from enum import Enum
+
+class State(Enum):
+    INACTIVE = 0
+    ACTIVE = 1
+
+print(State.ACTIVE.value)
+print(State.INACTIVE.value)
+# print(list(State))
+# print(len(State))
+
+#Input
+# age = input("What is your age? ")
+# print(f"Your age is {age}")
+
+#Control Statements
+condition = True
+
+if condition:
+    print("The condition")
+    print("was true")
+# elif <condition>:
+else:
+    print("The condition was false")
+
+# List
+dogs = [] #Empty List
+dogs = ["Roger", 1, "Syd", True]
+
+# print("Roger" in dogs) #True
+# print(dogs[2]) "Syd"
+dogs[2] = "Beau"
+
+# print(dogs[-1]) #True
+# print(dogs[2:4]) #Beau, True
+# print(dogs[:3]) #Roger, 1, Beau
+# print(dogs[2:]) #Beau, True
+# print(len(dogs)) #4
+# dogs.append("Lucky") #add Lucky on list
+# dogs.extend(["Lucky", 7]) #add list on dogs
+# dogs += "[Panda, 8]" #same as extend
+# dogs.remove("Beau")
+# print(dogs.pop()) #get the last item of the list and remove
+# dogs.insert(2, "Pogi")
+# dogs[1:1] = ["Test1", "Test2"]
+print(dogs)
+
+items = ["Roger", "Panda", "Lylia", "Lucky", "pogi"]
+# itemscopy = items[:]
+
+#Sorting
+# items.sort(key=str.lower)
+print(sorted(items, key=str.lower))
+print(items)
+# print(itemscopy)
+
+#Tuples
